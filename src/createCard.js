ff9fb7eb071a6c15e0a8ca5a90b6d6c647695792
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-03-22 19:10:00 +0100

    correction

--- src/createCard.js
+++ src/createCard.js
@@ -1,5 +1,27 @@
-function createCard() {
-	// du code
+function createCard(data) {
+	let html = `
+  <!-- CARD EXAMPLE -->
+  <div class="card">
+      <div class="card-header">
+        <div class="card-img" style="background-image:url('${data.img}') "></div>
+      </div>
+    <div class="card-body">
+      <h2 class="card-title">${data.name}</h2>
+      <p class="card-description">${data.desc}</p>
+    <button class="card-button">I want it!</button>
+    </div>
+  </div>
+<!-- END OF THE CARD EXAMPLE -->`;
+
+	return html;
 }
 
-export default createCard;
+function displayHTML(houses) {
+	let html = "";
+	houses.forEach((house) => {
+		html += createCard(house);
+	});
+	return html;
+}
+
+export default displayHTML;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-// du code ici
+// index.js
+
+import displayHTML from "./createCard";
+import houseToRent from "./houseToRent";
+
+const CARDS = document.querySelector(".cards");
+const typeHouse = document.querySelector("select");
+const isAvailable = document.querySelector(".available-checkbox");
+const search = document.querySelector(".search-input");
+
+/** A quoi servent les variables ?
+ * A avoir une copie avant de la modifier
+ */
+let houseFilter = houseToRent;
+let houseAvailable = [];
+let houseSearch = [];
+
+/** Ici, on vérifie si le bouton est checked ou pas */
+isAvailable.addEventListener("change", (e) => {
+	if (e.target.checked) {
+		houseAvailable = houseFilter.filter((house) => house.available);
+	} else {
+		houseAvailable = houseFilter;
+	}
+	CARDS.innerHTML = displayHTML(houseAvailable);
+});
+
+/** Ici, c'est si le un filtre sur le type de la maison est communiqué */
+typeHouse.addEventListener("change", () => {
+	houseFilter = houseToRent.filter((house) => house.type === typeHouse.value);
+	if (typeHouse.value === "All") {
+		houseFilter = houseToRent;
+	}
+	CARDS.innerHTML = displayHTML(houseFilter);
+});
+
+/** Ici, c'est pour la recherche avec mot clé */
+search.addEventListener("keyup", () => {
+	let searchValue = search.value.toLowerCase();
+	houseSearch = houseFilter.filter((house) => {
+		return house.name.toLowerCase().includes(searchValue);
+	});
+	CARDS.innerHTML = displayHTML(houseSearch);
+});
+
+CARDS.innerHTML = displayHTML(houseFilter);
