22400d3912b230b65b77ef5e83adf0e75537fe7e
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-03-22 16:13:19 +0100

    add img

--- src/houseToRent.js
+++ src/houseToRent.js
@@ -17,6 +17,7 @@ const houseToRent = [
 		name: "Beautiful House",
 		type: "House",
 		desc: "This is the perfect house for you, come to visit it you'll love it ",
+		img: "https://images.adsttc.com/media/images/5de3/1ca6/3312/fda8/2a00/00b3/newsletter/001.jpg?1575165037",
 		available: false,
 	},
 	{
